fix mistake in scale computation

ldm/modules/encoders/modules.py
@@ -89,15 +89,15 @@ class OpenCLIP(nn.Module):
         """
             Calculate the projection of v along the direction of t
             params:
-                v: visual tokens predicted by clip image encoder, shape: (b, n, c)
-                t: text feature predicted by clip text encoder (argmax -1), shape: (b, c)
+                v: visual tokens from clip image encoder, shape: (b, n, c)
+                t: text features from clip text encoder (argmax -1), shape: (b, 1, c)
         """
-        image_features = v / v.norm(dim=2, keepdim=True)
-        text_features = t / t.norm(dim=2, keepdim=True)
+        # v = v / v.norm(dim=2, keepdim=True)
+        # t = t / t.norm(dim=2, keepdim=True)
 
-        text_features = text_features.permute(0, 2, 1)
-        proj = torch.bmm(image_features, text_features)
-        t_square = (text_features ** 2).sum(dim=1, keepdim=True)
+        t = t.permute(0, 2, 1)
+        proj = torch.bmm(v, t)
+        t_square = (t ** 2).sum(dim=1, keepdim=True)
         return proj / t_square
 
 
@@ -254,6 +254,7 @@ class FrozenOpenCLIPEmbedder(AbstractEncoder):
         x = x.permute(1, 0, 2)  # LND -> NLD
         x = self.model.ln_final(x)
         x = x[torch.arange(x.shape[0]), text.argmax(dim=-1)] @ self.model.text_projection
+        x = x / x.norm(dim=1, keepdim=True)
         return x.unsqueeze(1)
 
     def text_transformer_forward(self, x: torch.Tensor, attn_mask = None):

models/mapper.py
@@ -55,7 +55,8 @@ class PromptTransformer(nn.Module):
 
         self.proj_in = nn.Linear(input_dim, inner_dim)
         self.transformer = nn.ModuleList(
-            [MemoryEfficientSelfAttnBlock(query_dim=input_dim, dim_head=dim_head)] * n_layers
+            [MemoryEfficientSelfAttnBlock(query_dim=input_dim, dim_head=dim_head)
+            for i in range(n_layers)]
         )
         self.proj_out = nn.Linear(inner_dim, input_dim)
 
@@ -137,9 +138,9 @@ class PromptMapper(pl.LightningModule):
         """
         shifted_v = torch.roll(v, self.offset, dims=0)
 
-        shifted_scale = self.clip.calculate_scale(shifted_v, t)
         global_correct_scale = self.clip.calculate_scale(gap(v), t)
-        global_shifted_scale = self.clip.calculate_scale(gap(shifted_v), t)
+        shifted_scale = self.clip.calculate_scale(shifted_v, t)
+        global_shifted_scale = gap(shifted_scale)
 
         dscale = (global_correct_scale - global_shifted_scale) * (shifted_scale / global_shifted_scale)
         return shifted_v, dscale
@@ -173,8 +174,8 @@ class PromptMapper(pl.LightningModule):
         z, c_concat, image_features, arg_text_features, x, xrec, xc = out
         if exists(target_scale):
             scale = self.clip.calculate_scale(image_features, arg_text_features)
-            global_scale = self.clip.calculate_scale(gap(image_features), arg_text_features)
-            target_scale = torch.ones_like(global_scale, device=global_scale.device) * target_scale
+            global_scale = gap(scale)
+            # target_scale = torch.ones_like(global_scale, device=global_scale.device) * target_scale
             dscale = (target_scale - global_scale) * (scale / global_scale)
         else:
             # sampling during training
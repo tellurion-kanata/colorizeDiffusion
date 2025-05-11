f209609dd3d4842d6f09ea41aaa7d7596ae9b1be
tellurion <tellurion.kanata@gmail.com>
2025-05-11 18:18:11 +0900

    Add files via upload

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,9 +1,12 @@
 import json
+import random
 import zipfile
 import os.path as osp
 import warnings
 
 from glob import glob
+
+import torch
 from PIL import ImageFile
 from collections import namedtuple
 from functools import partial
@@ -17,6 +20,10 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 warnings.filterwarnings("ignore", category=UserWarning)                 # Too many RGBA warnings
 
 
+# for ai-generated data, we set aesthetic score to 6
+DEFAULT_AESTHETIC_SCORE = 6
+
+
 class TripletDataset(data.Dataset):
     def __init__(
             self,
@@ -25,6 +32,7 @@ class TripletDataset(data.Dataset):
             image_key = "color",
             control_key = "sketch",
             condition_key = "reference",
+            text_key = None,
             json_key = None,
             score_threshold = 5,
             minimum_image_size = 768,
@@ -34,6 +42,7 @@ class TripletDataset(data.Dataset):
         self.sketch_root = osp.join(dataroot, control_key)
         self.color_root = osp.join(dataroot, image_key)
         self.ref_root = osp.join(dataroot, condition_key)
+        self.text_root = osp.join(dataroot, text_key) if exists(text_key) else None
 
         self.load_image_list(dataroot, json_key, score_threshold, minimum_image_size)
         self.data_size = len(self)
@@ -49,27 +58,27 @@ class TripletDataset(data.Dataset):
                 d = json.load(open(jf, "r"))
                 img_dict.update(d)
 
-            self.image_files = [
+            self.data_items = [
                 osp.join(self.sketch_root, file) for file in img_dict
                 if check_json(img_dict[file], score_threshold, minimum_image_size)
             ]
 
         else:
-            self.image_files = [
+            self.data_items = [
                 file for ext in IMAGE_EXTENSIONS
                 for file in
                 glob(osp.join(self.sketch_root, f'*.{ext}')) + glob(osp.join(self.sketch_root, f'*/*.{ext}'))
             ]
 
     def get_images(self, index):
-        filename = self.image_files[index]
+        filename = self.data_items[index]
 
         ske = Image.open(filename).convert('RGB')
         col = Image.open(filename.replace(self.sketch_root, self.color_root)).convert('RGB')
         w, h = col.size
 
         if self.offset > 0:
-            ref_file = self.image_files[(index + self.offset) % self.data_size]
+            ref_file = self.data_items[(index + self.offset) % self.data_size]
             ref = Image.open(ref_file.replace(self.sketch_root, self.color_root)).convert('RGB')
         else:
             ref = Image.open(filename.replace(self.sketch_root, self.ref_root)).convert('RGB')
@@ -78,7 +87,7 @@ class TripletDataset(data.Dataset):
             "control": ske,
             "image": col,
             "reference": [ref, None],
-            "size": [h, w]
+            "size": [h, w],
         }
 
     def __getitem__(self, index):
@@ -86,7 +95,7 @@ class TripletDataset(data.Dataset):
 
 
     def __len__(self):
-        return len(self.image_files)
+        return len(self.data_items)
 
 
 class ZipTripletDataset(TripletDataset):
@@ -100,27 +109,31 @@ class ZipTripletDataset(TripletDataset):
                 d = json.load(open(jf, "r"))
                 img_dict.update(d)
 
-            self.image_files = [
-                file for file in img_dict
-                if check_json(img_dict[file], score_threshold, minimum_image_size)
+            self.data_items = [
+                (
+                    file, 
+                    img_dict[file].get("aesthetic_score", DEFAULT_AESTHETIC_SCORE),
+                    img_dict[file].get("tags", "")
+                )
+                for file in img_dict if check_json(img_dict[file], score_threshold, minimum_image_size)
             ]
 
             # Add high-resolution generated image
-            self.image_files += [file for zip in glob(osp.join(self.sketch_root, "ai*.zip"))
-                                 for file in zipfile.ZipFile(zip).namelist() if not file.endswith("/")]
+            # self.data_items += [(file, DEFAULT_AESTHETIC_SCORE) for zip in glob(osp.join(self.sketch_root, "ai*.zip"))
+            #                      for file in zipfile.ZipFile(zip).namelist() if not file.endswith("/")]
 
         else:
-            self.image_files = [file for zip in glob(osp.join(self.sketch_root, "*.zip"))
+            self.data_items = [(file, DEFAULT_AESTHETIC_SCORE, "") for zip in glob(osp.join(self.sketch_root, "*.zip"))
                                 for file in zipfile.ZipFile(zip).namelist() if not file.endswith("/")]
 
     def fresh_zip_files(self, zipid):
-        self.zip_sketch, self.zip_color = map(
-            lambda t: zipfile.ZipFile(osp.join(t, f"{zipid}.zip"), "r"),
-            (self.sketch_root, self.color_root)
+        self.zip_sketch, self.zip_color, self.zip_text = map(
+            lambda t: zipfile.ZipFile(osp.join(t, f"{zipid}.zip"), "r") if exists(t) else None,
+            (self.sketch_root, self.color_root, self.text_root)
         )
 
     def get_images(self, index):
-        filename = self.image_files[index]
+        filename, aesthetic_score, tags = self.data_items[index]
         self.fresh_zip_files(osp.dirname(filename))
 
         ske = Image.open(self.zip_sketch.open(filename, "r")).convert('RGB')
@@ -128,15 +141,22 @@ class ZipTripletDataset(TripletDataset):
         w, h = col.size
 
         if self.offset > 0:
-            filename = self.image_files[(index + self.offset) % self.data_size]
+            filename, aesthetic_score, rtags = self.data_items[(index + self.offset) % self.data_size]
             self.fresh_zip_files(osp.dirname(filename))
         ref = Image.open(self.zip_color.open(filename, "r")).convert('RGB')
+
+        if exists(self.zip_text):
+            text = self.zip_text.open(filename, "r").read().decode('utf-8')
+        else:
+            text = None
 
         return {
             "control": ske,
             "image": col,
             "reference": [ref, None],
-            "size": [h, w]
+            "size": [h, w],
+            "text": text if exists(text) else tags,
+            "aesthetic_score": torch.tensor(aesthetic_score)
         }
 
     def __del__(self):
@@ -147,74 +167,6 @@ class ZipTripletDataset(TripletDataset):
         finally:
             self.zip_sketch = None
             self.zip_color = None
-
-
-class ZipTripletTextDataset(TripletDataset):
-    def __init__(
-            self,
-            dataroot,
-            mode = "train",
-            condition_key = "reference",
-            *args,
-            **kwargs
-    ):
-        super().__init__(mode=mode, *args, **kwargs)
-
-        dataroot = osp.abspath(dataroot)
-        self.text_root = osp.join(dataroot, condition_key)
-        self.image_files = [file for zip in glob(osp.join(self.text_root, "*.zip"))
-                            for file in zipfile.ZipFile(zip).namelist() if not file.endswith("/")]
-
-        self.data_size = len(self)
-        self.offset = random.randint(0, self.data_size) if mode == 'validation' else 0
-
-        self.zip_color = None
-        self.zip_sketch = None
-        self.zip_text = None
-
-    def fresh_zip_files(self, zipid):
-        self.zip_sketch, self.zip_color, self.zip_text = map(
-            lambda t: zipfile.ZipFile(osp.join(t, f"{zipid}.zip"), "r"),
-            (self.sketch_root, self.color_root, self.text_root)
-        )
-
-    def get_inputs(self, index):
-        filename = self.image_files[index]
-        self.fresh_zip_files(osp.dirname(filename))
-
-        with self.zip_text.open(filename, "r") as f:
-            img_json = json.load(f)
-        ext = osp.splitext(img_json["img_link"])[-1]
-        filename = filename.replace(".json", ext)
-
-        txt = img_json["tags"]
-        ske = Image.open(self.zip_sketch.open(filename, "r")).convert('RGB')
-        col = Image.open(self.zip_color.open(filename, "r")).convert('RGB')
-        ref = col
-
-        return {
-            "control": ske,
-            "image": col,
-            "reference": ref,
-            "text": txt,
-        }
-
-    def __getitem__(self, index):
-        return self.get_inputs(index)
-
-    def __len__(self):
-        return len(self.image_files)
-
-    def __del__(self):
-        try:
-            if exists(self.zip_color) and exists(self.zip_sketch) and exists(self.zip_text):
-                self.zip_sketch.close()
-                self.zip_color.close()
-                self.zip_text.close()
-        finally:
-            self.zip_sketch = None
-            self.zip_color = None
-            self.zip_text = None
 
 
 class QuartetDataset(ZipTripletDataset):
@@ -228,13 +180,13 @@ class QuartetDataset(ZipTripletDataset):
         self.mask_root = osp.join(osp.abspath(dataroot), "mask")
 
     def fresh_zip_files(self, zipid):
-        self.zip_sketch, self.zip_color, self.zip_mask = map(
-            lambda t: zipfile.ZipFile(osp.join(t, f"{zipid}.zip"), "r"),
-            (self.sketch_root, self.color_root, self.mask_root)
+        self.zip_sketch, self.zip_color, self.zip_mask, self.zip_text = map(
+            lambda t: zipfile.ZipFile(osp.join(t, f"{zipid}.zip"), "r") if exists(t) else None,
+            (self.sketch_root, self.color_root, self.mask_root, self.text_root)
         )
 
     def get_images(self, index):
-        filename = self.image_files[index]
+        filename, aesthetic_score, tags = self.data_items[index]
         self.fresh_zip_files(osp.dirname(filename))
 
         ske = Image.open(self.zip_sketch.open(filename, "r")).convert('RGB')
@@ -243,9 +195,14 @@ class QuartetDataset(ZipTripletDataset):
         w, h = col.size
 
         if self.offset > 0:
-            filename = self.image_files[(index + self.offset) % self.data_size]
+            filename, aesthetic_score, rtags = self.data_items[(index + self.offset) % self.data_size]
             self.fresh_zip_files(osp.dirname(filename))
         ref = Image.open(self.zip_color.open(filename, "r")).convert('RGB')
+
+        if exists(self.zip_text):
+            text = self.zip_text.open(filename, "r").read().decode('utf-8')
+        else:
+            text = None
 
         return {
             "control": ske,
@@ -254,6 +211,8 @@ class QuartetDataset(ZipTripletDataset):
             "smask": mask,
             "rmask": mask,
             "size": [h, w],
+            "text": text if exists(text) else tags,
+            "aesthetic_score": torch.tensor(aesthetic_score)
         }
 
     def __getitem__(self, index):
@@ -263,7 +222,7 @@ class QuartetDataset(ZipTripletDataset):
 
             except Exception as e:
                 index += 1
-                print(f"Cannot open file {self.image_files[index]} due to {e} !!!")
+                print(f"Cannot open file {self.data_items[index]} due to {e} !!!")
     
     def __del__(self):
         try:
@@ -290,7 +249,7 @@ class CharacterDataset(QuartetDataset):
                 return refname
 
     def get_images(self, index):
-        filename = self.image_files[index]
+        filename, _, tags = self.data_items[index]
         refname = self.fresh_zip_files(osp.dirname(filename))
         refname = refname or filename
 
@@ -310,6 +269,7 @@ class CharacterDataset(QuartetDataset):
             "smask": smask,
             "rmask": rmask,
             "size": [h, w, rw, rh],
+            "aesthetic_score": torch.tensor(DEFAULT_AESTHETIC_SCORE)
         }
 
 
@@ -327,15 +287,16 @@ class CustomCollateFn:
             keep_ratio = False,
             inverse_grayscale = False,
             eval_load_size = 768,
-            random_erase = False,
-            mask_expansion = False,
+            erase_p = 0.,
+            mask_expansion_p = 0.,
             mask_expansion_size = (60, 40),
             eval = False
     ):
         self.inverse_grayscale = inverse_grayscale
 
         if not eval:
-            self.mask_expansion = mask_expansion
+            self.mask_expansion_p = mask_expansion_p
+            self.erase_p = erase_p
             self.mask_expansion_size = mask_expansion_size
             self.preprocess = self.training_preprocess
 
@@ -355,7 +316,6 @@ class CustomCollateFn:
             self.ref_transforms = partial(custom_transform, t=transforms)
             self.center_crop_max = center_crop_max
             self.image_size = crop_size
-            self.erase = random_erase
 
         else:
             self.preprocess = self.testing_preprocess
@@ -392,9 +352,9 @@ class CustomCollateFn:
                 smask = to_tensor(self.gt_transforms(smask, *gt_seeds))
                 rmask = to_tensor(self.ref_transforms(rmask, *ref_seeds))
 
-                if self.mask_expansion:
+                if self.mask_expansion_p > 0 and random.rand() < self.mask_expansion_p:
                     rmask = mask_expansion(rmask, *self.mask_expansion_size)
-                elif self.erase:
+                elif self.erase_p > 0 and random.rand() < self.erase_p:
                     rmask = transforms.RandomErasing(
                         p=0.5, scale=(0.2, 0.5), ratio=(0.2, 3), value=1, inplace=True
                     )(rmask)
@@ -412,9 +372,9 @@ class CustomCollateFn:
 
                 if exists(omask):
                     omask = to_tensor(self.ref_transforms(omask, *ref_seeds))
-                    if self.mask_expansion:
+                    if self.mask_expansion_p > 0 and random.rand() < self.mask_expansion_p:
                         omask = mask_expansion(omask, *self.mask_expansion_size)
-                    elif self.erase:
+                    elif self.erase_p > 0 and random.rand() < self.erase_p:
                         omask = transforms.RandomErasing(
                             p=0.5, scale=(0.1, 0.2), ratio=(0.33, 3), value=1, inplace=True
                         )(omask)
@@ -427,7 +387,8 @@ class CustomCollateFn:
                 inputs["reference"][i] = normalize(ref)
 
         for k in inputs:
-            inputs[k] = torch.stack(inputs[k])
+            if k not in ["tag", "text"]:
+                inputs[k] = torch.stack(inputs[k])
         return inputs
 
     def testing_preprocess(self, batch):
@@ -438,12 +399,15 @@ class CustomCollateFn:
             inputs[k] = [item[k] for item in batch]
 
         for i in range(len(batch)):
-            ske = resize_and_pad(batch[i]["control"], self.image_size, self.image_size)
-            col = resize(batch[i]["image"], self.image_size)
-            ref = resize(batch[i]["reference"][0], self.image_size)
+            item = batch[i]
+
+            ske = resize_and_pad(item["control"], self.image_size, self.image_size)
+            col = resize(item["image"], self.image_size)
+            ref = resize(item["reference"][0], self.image_size)
             inputs["control"][i] = -normalize(ske) if self.inverse_grayscale else normalize(ske)
             inputs["image"][i] = normalize(col)
             inputs["reference"][i] = normalize(ref)
+            inputs["size"][i] = torch.Tensor(item["size"] + [self.image_size])
 
         for k in inputs:
             inputs[k] = torch.stack(inputs[k])
@@ -458,7 +422,6 @@ def create_dataloader(opt, cfg, device_num, eval_load_size=None):
         "TripletLoader": TripletDataset,
         "ZipTripletLoader": ZipTripletDataset,
         "QuartLoader": QuartetDataset,
-        "TextLoader": ZipTripletTextDataset,
         "CharacterLoader": CharacterDataset,
     }
 
